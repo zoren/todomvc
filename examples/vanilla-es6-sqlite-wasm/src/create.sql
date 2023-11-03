8542f12a4a10507eb8a93f69ba1a89d29f5fe8f1
Søren Sjørup <soren@zoren.dk>
2023-11-03 18:15:12 +0100

    make view temp again

--- examples/vanilla-es6-sqlite-wasm/src/create.sql
+++ examples/vanilla-es6-sqlite-wasm/src/create.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS todos (
 CREATE INDEX IF NOT EXISTS completed_index ON todos (completed);
 
 -- we will need to count items often so we create a view
-CREATE VIEW IF NOT EXISTS todo_counts AS 
+CREATE TEMP VIEW todo_counts AS 
 	SELECT
 		(SELECT COUNT() FROM todos WHERE completed = 0) as active_count,
 		(SELECT COUNT() FROM todos) as total_count;
