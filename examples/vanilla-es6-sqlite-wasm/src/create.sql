128b915cea4fe1eeeba098cb9583e20767e6cb6d
Søren Sjørup <soren@zoren.dk>
2023-11-04 11:37:22 +0100

    remove unused view

--- examples/vanilla-es6-sqlite-wasm/src/create.sql
+++ examples/vanilla-es6-sqlite-wasm/src/create.sql
@@ -8,9 +8,3 @@ CREATE TABLE IF NOT EXISTS todos (
 
 -- we will need to filter on completed so we create an index
 CREATE INDEX IF NOT EXISTS completed_index ON todos (completed);
-
--- we will need to count items often so we create a view
-CREATE TEMP VIEW todo_counts AS 
-	SELECT
-		(SELECT COUNT() FROM todos WHERE completed = 0) as active_count,
-		(SELECT COUNT() FROM todos) as total_count;
