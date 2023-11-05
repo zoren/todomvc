f4c798a1cba0f478bffaf49a7c1744ae5f699816
Søren Sjørup <soren@zoren.dk>
2023-11-05 17:16:22 +0100

    inline create script

--- examples/vanilla-es6-sqlite-wasm/src/controller.js
+++ examples/vanilla-es6-sqlite-wasm/src/controller.js
@@ -1,4 +1,3 @@
-import databaseCreateScript from './create.sql?raw';
 import View from './view.js';
 
 const selectItemTitle = (db, $id) =>
@@ -73,7 +72,17 @@ export default class Controller {
 			0 // passed in as _ctxPtr
 		);
 
-		ooDB.exec(databaseCreateScript);
+		ooDB.exec(`
+CREATE TABLE IF NOT EXISTS todos (
+	title TEXT NOT NULL,
+	completed INTEGER NOT NULL DEFAULT 0,
+	CHECK (title <> ''), -- empty titles are not alliowed in TodoMVC
+	CHECK (completed IN (0, 1)) -- SQLite uses 0 and 1 for booleans, let check they are
+);
+
+-- we will need to filter on completed so we create an index
+CREATE INDEX IF NOT EXISTS completed_index ON todos (completed);		
+		`);
 
 		// insert item trigger
 		ooDB.createFunction(
--- examples/vanilla-es6-sqlite-wasm/src/create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS todos (
-	title TEXT NOT NULL,
-	completed INTEGER NOT NULL DEFAULT 0,
-	CHECK (title <> ''), -- empty titles are not alliowed in TodoMVC
-	CHECK (completed IN (0, 1)) -- SQLite uses 0 and 1 for booleans, let check they are
-);
-
--- we will need to filter on completed so we create an index
-CREATE INDEX IF NOT EXISTS completed_index ON todos (completed);
